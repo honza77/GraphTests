2d3440527b15e1b8140609941e28afff07cd8b85
honza77 <henzl@post.cz>
2017-05-29 14:01:46 +0200

    + First SimpleExample of BidirectionalGraph

---
+++ GraphXTests/GraphXSampleLib/GraphSamplesFactory.cs
@@ -0,0 +1,22 @@
+﻿using QuickGraph;
+
+namespace GraphXSampleLib
+{
+    class GraphSamplesFactory
+    {
+
+        public BidirectionalGraph<int, IEdge<int>> FirstSimpleExample()
+        {
+            var graph = new BidirectionalGraph<int, IEdge<int>>();
+
+            graph.AddVertex(1);
+            graph.AddVertex(2);
+
+            var edge1 = new Edge<int>(1, 2);
+
+            graph.AddEdge(edge1);
+
+            return graph;
+        }
+    }
+}
